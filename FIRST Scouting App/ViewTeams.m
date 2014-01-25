5eb1d36894b6545f512261ea9d37f5bc3bd65924
Louie Bertoncin <louiebertoncin@gmail.com>
2014-01-25 00:31:59 -0600

    Getting the detail view for the pit team working! It’s going great!

--- FIRST Scouting App/ViewTeams.m
+++ FIRST Scouting App/ViewTeams.m
@@ -24,6 +24,10 @@ NSManagedObjectContext *context;
 CoreDataTableViewController *cdtvc;
 NSFetchedResultsController *frc;
 
+UIView *grayOutview;
+UIView *pitDetailView;
+CGRect selectedCellRect;
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil{
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
@@ -100,11 +104,81 @@ NSFetchedResultsController *frc;
     // but don’t worry, we’re going to make it easy on you ...
 }
 
--(void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath{
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     PitTeam *ptSelected = [frc objectAtIndexPath:indexPath];
-    UIAlertView *teamSelectedAlert = [[UIAlertView alloc] initWithTitle:[[NSString alloc] initWithFormat:@"You Selected %@", ptSelected.teamName] message:[[NSString alloc] initWithFormat:@"They have a %@ drivetrain!", ptSelected.driveTrain] delegate:nil cancelButtonTitle:@"Awesome! You Work!" otherButtonTitles: nil];
-    [teamSelectedAlert show];
-//    [_tableView deselectRowAtIndexPath:indexPath animated:YES];
+    selectedCellRect = [tableView convertRect:[tableView rectForRowAtIndexPath:indexPath] toView:[tableView superview]];
+    
+    grayOutview = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 768, 1024)];
+    grayOutview.backgroundColor = [UIColor colorWithWhite:0.5 alpha:0.5];
+    [self.view addSubview:grayOutview];
+    
+    pitDetailView = [[UIView alloc] initWithFrame:CGRectMake(109, 190, 550, 600)];
+    pitDetailView.layer.cornerRadius = 10;
+    pitDetailView.backgroundColor = [UIColor whiteColor];
+    [grayOutview addSubview:pitDetailView];
+    
+    UIButton *closeButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    closeButton.frame = CGRectMake(480, 15, 60, 15);
+    [closeButton setTitle:@"Close X" forState:UIControlStateNormal];
+    closeButton.titleLabel.font = [UIFont systemFontOfSize:12];
+    closeButton.titleLabel.textAlignment = NSTextAlignmentCenter;
+    [closeButton addTarget:self action:@selector(closeDetailView) forControlEvents:UIControlEventTouchUpInside];
+    
+    UILabel *teamNumberLbl = [[UILabel alloc] initWithFrame:CGRectMake(305, 70, 100, 40)];
+    teamNumberLbl.text = [[NSString alloc] initWithFormat:@"%@", ptSelected.teamNumber];
+    teamNumberLbl.font = [UIFont fontWithName:@"Futura-CondensedExtraBold" size:35.0f];
+    teamNumberLbl.textAlignment = NSTextAlignmentCenter;
+    
+    UILabel *teamNameLbl = [[UILabel alloc] initWithFrame:CGRectMake(255, 120, 200, 20)];
+    teamNameLbl.text = [[NSString alloc] initWithFormat:@"%@", ptSelected.teamName];
+    teamNameLbl.font = [UIFont fontWithName:@"HeitiSC-Light" size:15.0f];
+    teamNameLbl.adjustsFontSizeToFitWidth = true;
+    teamNameLbl.textAlignment = NSTextAlignmentCenter;
+    
+    UIImage *image = [UIImage imageWithData:[ptSelected valueForKey:@"image"]];
+    UIImageView *robotImage = [[UIImageView alloc] initWithFrame:CGRectMake(10, 10, 200, 200)];
+    robotImage.image = image;
+    
+    UILabel *driveTrainLbl = [[UILabel alloc] initWithFrame:CGRectMake(30, 240, 240, 25)];
+    driveTrainLbl.textAlignment = NSTextAlignmentCenter;
+    driveTrainLbl.text = [[NSString alloc] initWithFormat:@"Drive Train: %@", ptSelected.driveTrain];
+    driveTrainLbl.adjustsFontSizeToFitWidth = true;
+    driveTrainLbl.backgroundColor = [UIColor colorWithWhite:0.8 alpha:0.8];
+    driveTrainLbl.layer.cornerRadius = 5;
+    driveTrainLbl.font = [UIFont boldSystemFontOfSize:15];
+    
+    UILabel *shooterLbl = [[UILabel alloc] initWithFrame:CGRectMake(310, 240, 180, 25)];
+    shooterLbl.textAlignment = NSTextAlignmentCenter;
+    shooterLbl.text = [[NSString alloc] initWithFormat:@"Shooter: %@", ptSelected.shooter];
+    shooterLbl.backgroundColor = [UIColor colorWithWhite:0.8 alpha:0.8];
+    shooterLbl.layer.cornerRadius = 5;
+    shooterLbl.font = [UIFont boldSystemFontOfSize:15];
+    
+    pitDetailView.frame = CGRectMake(selectedCellRect.origin.x + 364, selectedCellRect.origin.y + 40, 1, 1);
+    [UIView animateWithDuration:0.2 animations:^{
+        pitDetailView.frame = CGRectMake(109, 190, 550, 600);
+    } completion:^(BOOL finished) {
+        [pitDetailView addSubview:closeButton];
+        [pitDetailView addSubview:teamNumberLbl];
+        [pitDetailView addSubview:teamNameLbl];
+        [pitDetailView addSubview:robotImage];
+        [pitDetailView addSubview:driveTrainLbl];
+        [pitDetailView addSubview:shooterLbl];
+    }];
+    
+    [_tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+-(void)closeDetailView{
+    for (UIView *v in [pitDetailView subviews]) {
+        [v removeFromSuperview];
+    }
+    [UIView animateWithDuration:0.2 animations:^{
+        pitDetailView.frame = CGRectMake(selectedCellRect.origin.x + 364, selectedCellRect.origin.y + 40, 1, 1);
+    } completion:^(BOOL finished) {
+        [pitDetailView removeFromSuperview];
+        [grayOutview removeFromSuperview];
+    }];
 }
 
 - (IBAction)searching:(id)sender {
